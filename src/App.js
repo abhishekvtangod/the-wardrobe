6160c8c5473b9a23fda1e78198d1b78bdb4eccdc
abhishekvtangod <abhishekvtangod@gmail.com>
2020-07-01 00:56:08 +0530

    updated app with shop page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const Hatspage = props => {
-  console.log(props)
-  return(
-      <div>
-        <Link to='/'>Homepage </Link>
-        <h1>HATS PAGE : {props.match.params.Id}</h1>
-      </div>
-  );
-};
+
 
 
 function App() {
   return (
     <div>
         <Route exact path = '/' component={HomePage}/>
-        <Route path='/shop/hats' component={Hatspage}/>
+        <Route path='/shop' component={ShopPage}/>
     </div>
   );
 }
